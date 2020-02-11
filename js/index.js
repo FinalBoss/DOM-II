b72bdc39a834ff9c7ee91b15495827301132a547
Brian Clayton <finalb@projectfinalboss.com>
2020-02-11 18:20:40 -0500

    change bg settings

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+
+        //Change Bg
+
+ let bg = document.querySelector('body');
+let bgFoot =  document.querySelector('.footer');
+let bgNav = document.querySelector('.main-navigation');
+
+//  bg.style.background = 'black';
+//  bgFoot.style.background = 'black';
+//  bgNav.style.background = 'black';
+
+
+
+            //Change Text Color
+
+
+
+
+
+
